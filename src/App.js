f424a08a20fc04fe82b147fd3bfe0f4ce481ddfd
honorific <honorific.info@gmail.com>
2023-04-25 12:03:34 +0430

    show price and data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [price, setPrice] = useState(-1)
+  const [priceTime, setPriceTime] = useState(null)
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const stonkUrl = `${proxyUrl}https://query1.finance.yahoo.com/v8/finance/chart/GME`
+
   const getStonk = async () => {
     const response = await fetch(stonkUrl)
     return response.json()
   }
   useEffect(() => {
-    getStonk().then((data) => console.log(data))
+    let timeId
+    const getLatestPrice = async () => {
+      const data = await getStonk()
+      console.log(data)
+      const gme = data.chart.result[0]
+      console.log(gme)
+      setPrice(gme.meta.regularMarketPrice.toFixed(2))
+      setPriceTime(new Date(gme.meta.regularMarketTime * 1000))
+      timeId = setTimeout(getLatestPrice, 5000)
+    }
+
+    timeId = setTimeout(getLatestPrice, 5000)
+    //getLatestPrice()
+
+    return () => {
+      clearTimeout(timeId)
+    }
   }, [])
 
-  return <div>hello world</div>
+  return (
+    <div className='price'>
+      {price}
+      <br />
+      {priceTime?.toLocaleTimeString()}
+    </div>
+  )
 }
 
 export default App
